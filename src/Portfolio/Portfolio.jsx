6cc55fbefb275fcf1f93d9fa608fa9d4e735af7c
Jonathan Massot <jonathan.massot.j@gmail.com>
2022-07-04 09:57:13 +0800

    deleted useless files, updated icon

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -11,9 +11,9 @@ const Portfolio = () => {
       <h2>A rich portfolio of experiences, events and performances</h2>
       <PickACard />
       <Logos />
-      <div className="view-all">
+      {/* <div className="view-all">
         <Link id="view-all-link" to="/all">View all</Link>
-      </div>
+      </div> */}
     </div>
   )
 }
